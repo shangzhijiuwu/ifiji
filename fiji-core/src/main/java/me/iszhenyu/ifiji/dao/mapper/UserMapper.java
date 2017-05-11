5539b087edf140c4ce8b2bf306d229947d8cc49d
zhen.yu <iszhenyu@yeah.net>
2017-05-11 21:52:53 +0800

    ..

--- fiji-core/src/main/java/me/iszhenyu/ifiji/dao/mapper/UserMapper.java
+++ fiji-core/src/main/java/me/iszhenyu/ifiji/dao/mapper/UserMapper.java
@@ -1,9 +1,19 @@
 package me.iszhenyu.ifiji.dao.mapper;
 
 import me.iszhenyu.ifiji.model.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * Created by xiaoz on 2017/5/11.
  */
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
+
+	@Select("select * from sys_user where mobile_number = #{mobile}")
+	User getByMobileNumber(@Param("mobile") String mobile);
+
+	@Select("select * from sys_user where username = #{username}")
+	User getByUsername(@Param("username") String username);
 }
