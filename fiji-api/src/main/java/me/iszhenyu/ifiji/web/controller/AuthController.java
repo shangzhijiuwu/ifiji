247a8e2998b3b010749750e105b452b63d740afd
zhen.yu <iszhenyu@yeah.net>
2017-05-11 22:03:37 +0800

    ..

---
+++ fiji-api/src/main/java/me/iszhenyu/ifiji/web/controller/AuthController.java
@@ -0,0 +1,7 @@
+package me.iszhenyu.ifiji.web.controller;
+
+/**
+ * Created by xiaoz on 2017/5/11.
+ */
+public class AuthController {
+}
---
+++ fiji-api/src/main/java/me/iszhenyu/ifiji/web/form/LoginForm.java
@@ -0,0 +1,7 @@
+package me.iszhenyu.ifiji.web.form;
+
+/**
+ * Created by xiaoz on 2017/5/11.
+ */
+public class LoginForm {
+}
