49ba1b4f763f695dd20cf09cd470c975bcf3d9a2
zhen.yu <iszhenyu@yeah.net>
2017-05-11 22:12:45 +0800

    ..

--- fiji-api/src/main/java/me/iszhenyu/ifiji/web/controller/UserController.java
+++ fiji-api/src/main/java/me/iszhenyu/ifiji/web/controller/UserController.java
@@ -1,6 +1,5 @@
 package me.iszhenyu.ifiji.web.controller;
 
-import me.iszhenyu.ifiji.model.User;
 import me.iszhenyu.ifiji.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,25 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by xiaoz on 2017/5/11.
  */
 @RestController
-@RequestMapping("/auth")
-public class AuthController {
+@RequestMapping("/user")
+public class UserController {
 
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login() {
-		return "login";
-	}
-
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public User doLogin() {
-		return null;
-	}
-
-	@RequestMapping("/logout")
-	public String logout() {
-		return null;
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() {
+		return "test";
 	}
 
 }
