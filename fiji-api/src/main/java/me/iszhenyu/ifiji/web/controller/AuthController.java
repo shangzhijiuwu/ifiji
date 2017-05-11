99c31a002a5e1a71c67fd5370d212ad0904b8691
zhen.yu <iszhenyu@yeah.net>
2017-05-11 22:06:25 +0800

    ..

--- fiji-api/src/main/java/me/iszhenyu/ifiji/web/controller/AuthController.java
+++ fiji-api/src/main/java/me/iszhenyu/ifiji/web/controller/AuthController.java
@@ -1,7 +1,20 @@
 package me.iszhenyu.ifiji.web.controller;
 
+import me.iszhenyu.ifiji.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by xiaoz on 2017/5/11.
  */
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+	@Autowired
+	private UserService userService;
+
+
+
 }
