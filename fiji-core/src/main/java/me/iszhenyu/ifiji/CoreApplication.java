6daf60460e9e567601b3adc4de6ebc71721a27ef
zhen.yu <iszhenyu@yeah.net>
2017-06-05 10:06:45 +0800

    ..

--- fiji-core/src/main/java/me/iszhenyu/ifiji/CoreApplication.java
+++ fiji-core/src/main/java/me/iszhenyu/ifiji/CoreApplication.java
@@ -4,6 +4,9 @@ import me.iszhenyu.ifiji.dao.interceptor.AutoDateTimeInterceptor;
 import org.apache.ibatis.plugin.Interceptor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
 
 /**
  * Created by xiaoz on 2017/5/21.
@@ -14,6 +17,11 @@ public class CoreApplication {
 	@Bean
 	public Interceptor autoDateTimeInterceptor() {
 		return new AutoDateTimeInterceptor();
+	}
+
+	@Bean
+	public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
 	}
 
 }
