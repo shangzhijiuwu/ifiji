161c574e1413aa7c4fb6bed2843c9a099428ca79
zhen.yu <iszhenyu@yeah.net>
2017-06-09 18:27:45 +0800

    ..

--- fiji-api/src/main/java/me/iszhenyu/ifiji/web/controller/UserController.java
+++ fiji-api/src/main/java/me/iszhenyu/ifiji/web/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseController {
 
 	@Autowired
 	private UserService userService;
