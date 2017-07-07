a5536ccb21d1ff444fdc01922439a98f14dfeb79
zhen.yu <iszhenyu@yeah.net>
2017-07-07 13:08:15 +0800

    ..

--- fiji-api/src/main/java/me/iszhenyu/ifiji/web/controller/FijiExceptionHandler.java
+++ fiji-api/src/main/java/me/iszhenyu/ifiji/web/controller/FijiExceptionHandler.java
@@ -1,11 +1,16 @@
 package me.iszhenyu.ifiji.web.controller;
 
 import me.iszhenyu.ifiji.exception.FijiException;
-import me.iszhenyu.ifiji.web.vo.ErrorVO;
+import me.iszhenyu.ifiji.web.vo.ResponseVO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * @author zhen.yu
@@ -15,18 +20,48 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class FijiExceptionHandler {
 
     @ExceptionHandler({FijiException.class})
-    public ResponseEntity<ErrorVO> handleFijiException(FijiException e) {
-        ErrorVO restError = new ErrorVO();
-        restError.setStatus(e.getStatus().value());
-        restError.setErrorMessage(e.getShowMessage());
-        return new ResponseEntity<>(restError, e.getStatus());
+    public ResponseEntity<ResponseVO> handleFijiException(FijiException e) {
+        ResponseVO vo = new ResponseVO().failure(e.getShowMessage());
+        return new ResponseEntity<>(vo, e.getStatus());
     }
 
+    /**
+     * 400 - Bad Request
+     */
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseVO handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return new ResponseVO().failure("could_not_read_json");
+    }
+
+    /**
+     * 405 - Method Not Allowed
+     */
+    @ResponseBody
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseVO handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        return new ResponseVO().failure("request_method_not_supported");
+    }
+
+    /**
+     * 415 - Unsupported Media Type
+     */
+    @ResponseBody
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseVO handleHttpMediaTypeNotSupportedException(Exception e) {
+        return new ResponseVO().failure("content_type_not_supported");
+    }
+
+    /**
+     * 500 - Internal Server Error
+     */
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler({RuntimeException.class})
-    public ResponseEntity<ErrorVO> handleRuntimeException(RuntimeException e) {
-        ErrorVO restError = new ErrorVO();
-        restError.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        restError.setErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
-        return new ResponseEntity<>(restError, HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseVO handleRuntimeException(RuntimeException e) {
+        return new ResponseVO().failure(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
     }
 }
