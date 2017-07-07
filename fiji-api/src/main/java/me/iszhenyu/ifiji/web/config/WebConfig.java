a72285abee9973a30b1f56d8ad573430c07e1f0f
zhen.yu <iszhenyu@yeah.net>
2017-07-07 12:58:21 +0800

    ..

--- fiji-api/src/main/java/me/iszhenyu/ifiji/web/config/WebConfig.java
+++ fiji-api/src/main/java/me/iszhenyu/ifiji/web/config/WebConfig.java
@@ -1,7 +1,10 @@
 package me.iszhenyu.ifiji.web.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
@@ -11,4 +14,6 @@ import java.util.List;
  */
 //@Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+
 }
