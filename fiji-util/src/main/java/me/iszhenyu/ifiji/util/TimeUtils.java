44aedfc620f641dd4ec59c8d1479952c66775270
zhen.yu <iszhenyu@yeah.net>
2017-08-11 23:10:00 +0800

    ..

--- fiji-util/src/main/java/me/iszhenyu/ifiji/util/TimeUtils.java
+++ fiji-util/src/main/java/me/iszhenyu/ifiji/util/TimeUtils.java
@@ -2,7 +2,9 @@ package me.iszhenyu.ifiji.util;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 /**
@@ -20,13 +22,79 @@ public class TimeUtils {
     }
 
     public static Date gmtNow() {
-        Date d = new Date();
-        System.out.println(GMT_NORMAL_FORMAT.format(d));
-        return d;
+        Calendar cal = GregorianCalendar.getInstance(TimeZone.getTimeZone("GMT"));
+        return cal.getTime();
     }
 
-    public static void main(String[] args) {
-        TimeUtils.gmtNow();
+    /**
+     * Returns the days between two dates. Positive values indicate that
+     * the second date is after the first, and negative values indicate, well,
+     * the opposite. Relying on specific times is problematic.
+     *
+     * @param early the "first date"
+     * @param late the "second date"
+     * @return the days between the two dates
+     */
+    public static int daysBetween(Date early, Date late) {
+        Calendar c1 = Calendar.getInstance();
+        Calendar c2 = Calendar.getInstance();
+        c1.setTime(early);
+        c2.setTime(late);
+        return daysBetween(c1, c2);
+    }
+
+    /**
+     * Returns the days between two dates. Positive values indicate that
+     * the second date is after the first, and negative values indicate, well,
+     * the opposite.
+     *
+     * @param early
+     * @param late
+     * @return the days between two dates.
+     */
+    public static int daysBetween(Calendar early, Calendar late) {
+        return (int) (toJulian(late) - toJulian(early));
+    }
+
+    /**
+     * Return a Julian date based on the input parameter. This is
+     * based from calculations found at
+     * <a href="http://quasar.as.utexas.edu/BillInfo/JulianDatesG.html">Julian Day Calculations
+     * (Gregorian Calendar)</a>, provided by Bill Jeffrys.
+     * @param c a calendar instance
+     * @return the julian day number
+     */
+    public static float toJulian(Calendar c) {
+        int Y = c.get(Calendar.YEAR);
+        int M = c.get(Calendar.MONTH);
+        int D = c.get(Calendar.DATE);
+        int A = Y / 100;
+        int B = A / 4;
+        int C = 2 - A + B;
+        float E = (int) (365.25f * (Y + 4716));
+        float F = (int) (30.6001f * (M + 1));
+        return C + D + E + F - 1524.5f;
+    }
+
+    public static Date dateIncreaseByDay(Date date, int days) {
+        Calendar cal = GregorianCalendar.getInstance(TimeZone.getTimeZone("GMT"));
+        cal.setTime(date);
+        cal.add(Calendar.DATE, days);
+        return cal.getTime();
+    }
+
+    public static Date dateIncreaseByMonth(Date date, int mnt) {
+        Calendar cal = GregorianCalendar.getInstance(TimeZone.getTimeZone("GMT"));
+        cal.setTime(date);
+        cal.add(Calendar.MONTH, mnt);
+        return cal.getTime();
+    }
+
+    public static Date dateIncreaseByYear(Date date, int mnt) {
+        Calendar cal = GregorianCalendar.getInstance(TimeZone.getTimeZone("GMT"));
+        cal.setTime(date);
+        cal.add(Calendar.YEAR, mnt);
+        return cal.getTime();
     }
 
 }
