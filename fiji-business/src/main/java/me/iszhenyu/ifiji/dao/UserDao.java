c3c6ff819cbaf9a329b33b9c0f1186b8caaf3b3f
zhen.yu <iszhenyu@yeah.net>
2017-09-07 19:44:59 +0800

    ..

--- fiji-business/src/main/java/me/iszhenyu/ifiji/dao/UserDao.java
+++ fiji-business/src/main/java/me/iszhenyu/ifiji/dao/UserDao.java
@@ -5,12 +5,15 @@ import me.iszhenyu.ifiji.core.dao.BaseMapper;
 import me.iszhenyu.ifiji.dao.mapper.UserMapper;
 import me.iszhenyu.ifiji.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by xiaoz on 2017/5/11.
  */
 @Component
+@CacheConfig(cacheNames = "user")
 public class UserDao extends BaseDao<User> {
 
 	@Autowired
@@ -21,14 +24,17 @@ public class UserDao extends BaseDao<User> {
 		return userMapper;
 	}
 
+	@Cacheable
 	public User getByMobile(String mobile) {
 		return userMapper.getByMobileNumber(mobile);
 	}
 
+	@Cacheable
 	public User getByUsername(String username) {
 		return userMapper.getByUsername(username);
 	}
 
+	@Cacheable
 	public User getByEmail(String email) {
 		return userMapper.getByEmail(email);
 	}
--- fiji-business/src/main/java/me/iszhenyu/ifiji/service/UserService.java
+++ fiji-business/src/main/java/me/iszhenyu/ifiji/service/UserService.java
@@ -6,7 +6,6 @@ import me.iszhenyu.ifiji.model.User;
 import me.iszhenyu.ifiji.util.RandomUtils;
 import me.iszhenyu.ifiji.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 
