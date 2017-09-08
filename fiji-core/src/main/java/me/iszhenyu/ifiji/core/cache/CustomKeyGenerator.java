a8e3b752717105971a77da05778ae5785334c009
zhen.yu <iszhenyu@yeah.net>
2017-09-08 14:58:23 +0800

    ..

---
+++ fiji-core/src/main/java/me/iszhenyu/ifiji/core/cache/CustomKeyGenerator.java
@@ -0,0 +1,35 @@
+package me.iszhenyu.ifiji.core.cache;
+
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleKey;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author zhen.yu
+ * @since 2017/9/8
+ */
+public class CustomKeyGenerator implements KeyGenerator {
+
+    @Override
+    public Object generate(Object target, Method method, Object... params) {
+        return String.format("%s:%s:%s",
+                target.getClass().getName(),
+                method.getName(),
+                generateKey(params).toString()
+        );
+    }
+
+    private static Object generateKey(Object... params) {
+        if (params.length == 0) {
+            return SimpleKey.EMPTY;
+        }
+        if (params.length == 1) {
+            Object param = params[0];
+            if (param != null && !param.getClass().isArray()) {
+                return param;
+            }
+        }
+        return new SimpleKey(params);
+    }
+}
--- fiji-core/src/main/java/me/iszhenyu/ifiji/core/cache/RedisCacheConfiguration.java
+++ fiji-core/src/main/java/me/iszhenyu/ifiji/core/cache/RedisCacheConfiguration.java
@@ -1,7 +1,8 @@
 package me.iszhenyu.ifiji.core.cache;
 
 import me.iszhenyu.ifiji.core.cache.redis.JsonRedisSerializer;
-import org.springframework.cache.jcache.config.JCacheConfigurerSupport;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
@@ -17,7 +18,7 @@ import java.net.UnknownHostException;
  * @since 2017/9/7
  */
 @Configuration
-public class RedisCacheConfiguration extends JCacheConfigurerSupport {
+public class RedisCacheConfiguration extends CachingConfigurerSupport {
 
     private class CustomRedisTemplate extends RedisTemplate<String, Object> {
 
@@ -47,4 +48,8 @@ public class RedisCacheConfiguration extends JCacheConfigurerSupport {
         return cacheManager;
     }
 
+    @Override
+    public KeyGenerator keyGenerator() {
+        return new CustomKeyGenerator();
+    }
 }
